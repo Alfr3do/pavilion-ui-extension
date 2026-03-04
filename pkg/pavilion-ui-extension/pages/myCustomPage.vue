28dfc2c17dda3f0d19b46ad0c00dd76de4803029
Alfr3do <alfre.pollux@gmail.com>
2026-03-04 09:17:06 -0500

    first compilation

---
+++ pkg/pavilion-ui-extension/pages/myCustomPage.vue
@@ -0,0 +1,14 @@
+<script>
+export default { name: 'MyCustomPage' , meta: {product: {type:String, required: true}}};
+</script>
+
+<template>
+  <div>
+    <h1>Top Level Product</h1>
+    <p>This is a custom VueJS page. You can render anything you want "{{ meta.product }}"</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
