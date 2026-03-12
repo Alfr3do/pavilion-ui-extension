9919914173270696e1e2b664529593362e08531f
Alfr3do <alfre.pollux@gmail.com>
2026-03-11 23:39:15 -0400

    Build Two pages

---
+++ pkg/pavilion-ui-extension/pages/coldFront.vue
@@ -0,0 +1,17 @@
+<script>
+export default { 
+  name: 'coldFront' , 
+  props: ['product']
+  };
+</script>
+
+<template>
+  <div>
+    <h1>Cold Front</h1>
+    <p><a href="http://go.fiu.edu/pavilion-lease">Go to Cold Front</a></p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pkg/pavilion-ui-extension/pages/harbor.vue
@@ -0,0 +1,17 @@
+<script>
+export default { 
+  name: 'Image Repository' , 
+  props: ['product']
+  };
+</script>
+
+<template>
+  <div>
+    <h1>Harbor</h1>
+    <p><a href="http://go.fiu.edu/pavilion-images">Go to Harbor</a></p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
