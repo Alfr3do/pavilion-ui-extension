2d5b1eb4bc7497f26df11aeba991ac082bc50155
Alfr3do <alfre.pollux@gmail.com>
2026-03-26 13:19:44 -0400

    add: iframe consumes space

--- pkg/pavilion-ui-extension/pages/coldFront.vue
+++ pkg/pavilion-ui-extension/pages/coldFront.vue
@@ -6,7 +6,7 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="coldfront-wrapper">
     <h1>Cold Front</h1>
     <p><a href="http://go.fiu.edu/pavilion-lease">Go to Cold Front</a></p>
     <div class="external-frame-container">
@@ -16,5 +16,25 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+  .coldfront-wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh; /* Adjust if you want it to be 100% of the parent instead */
+    width: 100%;
+  }
 
+  .external-frame-container {
+    flex-grow: 1; /* Fills all remaining vertical space below the header */
+    width: 100%;
+    position: relative;
+  }
+
+  .external-frame {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
 </style>
